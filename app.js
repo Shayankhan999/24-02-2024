fa243c73b09fd259a4ef3d0ea51761c9e59ae5ca
Shayankhan <shayankhan9148@gmail.com>
2024-02-29 14:10:45 +0500

    alerts

---
+++ app.js
@@ -0,0 +1,134 @@
+//===============CHAPTER 1================
+// Question 1
+
+
+// var Error = "Error! Please enter a valid password."
+// alert(Error)
+
+
+// Question 2
+
+// var brak = "Welcome to JS Land... \nHappy Coding!"
+// alert(brak)
+
+// Question 3
+
+// var chx = "Happy Coding!"
+// alert(chx)
+
+// var wel = "Welcome to JS Land..."
+// alert(wel)
+
+// Question 4
+
+// var JS = "Hello... I can run JS through my web browser's console"
+// alert(JS)
+
+
+// console.log("Hello... I can run JS through my web browser's console")
+//===============CHAPTER 2================
+
+// Question 1
+
+// alert("Jhone Doe")
+// var hey = ("22 year old")
+// alert(hey)
+// var mobile = ("Certified Mobile Application Development")
+// alert(mobile)
+
+// Question 2
+
+// var linebreak = ("PIZZA \nPIZZ \nPIZ \nPI \nP")
+// alert(linebreak)
+
+// ====Second Way=====
+
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")     
+
+// Question 3
+
+// var email = ("My email address is shayankhan9148@gmail.com")
+// alert(email)
+
+// ====Second Way=====
+
+// var email = ("shayankhan9148@gmail.com")
+// alert("my email address is " + email )
+
+// Question 4
+
+// var learn = ("I am trying to learn from the Book A Smarter \nway to learn Javascript")
+// alert(learn)
+
+// ====Second Way=====
+
+// var Smarter = ("A Smarter \nway to learn Javascript")
+// alert("I am trying to learn from the Book " + Smarter)
+
+// Question 5
+
+// document.write("yah! I can write HTML content through JavaScirpt")
+
+// ====Second Way=====
+
+// var browser = "yah! I can write HTML content through JavaScirpt"
+// document. write(browser)
+
+// Question 6
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+ 
+// ====Second Way=====
+// var icon = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+// alert(icon)
+
+//===============CHAPTER 3================
+
+// Question 1
+
+// alert("i am 22 year old")
+
+// ====Second Way=====
+// var age = ("22 years old")
+// alert("I am " + age )
+
+// Question 2
+
+// var visit = ("site")
+// var visitors = (" 14 times")
+// alert("you have visited this " + visit + visitors)
+
+// ====Second Way=====
+
+// alert("you have visited this site 14 times")
+
+// ====Third Way=====
+
+// var who = ("you have visited this ")
+// var visit = ("site ")
+// var timer = ("14 times")
+
+// alert(who + visit + timer)
+
+// Question 3
+
+// document.write("My Birth year is 2002 <br> Data type of my ddeclared variable is number")
+
+// var year = ("My Birth year is 2002 <br>" )
+// document.write(year + "Data type of my ddeclared variable is number")
+
+// Question 4
+
+// var storelink = ("www.xyzClothing.com")
+// var storename = (" XYZ Clothing Store")
+// var visitorname = (" <b> John Doe </b> ")
+// var order = (" <b> 5 </b> ")
+// var product = (" <b> T-shirts(s) </b>")
+
+// document.write( visitorname + "ordered" + order + product + " on" + storename )
+
+
+
+// ====Second Way=====
+// document.write("<b> John Doe </b> ordered <b> 5 T-shirt(s) </b> on XYZ Clothing store")
+// console.log("shayan khan")
